799002c5b3c4bd0b09cf7c370a9ba5e50fa5ac48
Neeraj Dalal <admin@nrjdalal.com>
2023-11-06 00:04:50 +0530

    github links to rdt.li

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Page() {
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
         <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
           <Link
-            href="https://twitter.com/nrjdalal_com"
+            href="https://rdt.li/lNB90I"
             className="rounded-2xl border bg-muted bg-slate-100 px-4 py-1.5 text-xs font-medium"
             target="_blank"
           >
