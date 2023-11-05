76cce9df823cfa6f065f1323a7526f5c2ec5d462
Neeraj Dalal <admin@nrjdalal.com>
2023-11-06 04:33:51 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default async function Page() {
               href="https://rdt.li/WdWIbR"
               target="_blank"
               rel="noreferrer"
-              className="rounded-md border px-8 py-2.5"
+              className="rounded-md bg-background border px-8 py-2.5"
             >
               GitHub
             </Link>
