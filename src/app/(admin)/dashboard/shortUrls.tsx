d485da0fd0a0cf9f672fd75a8b4f7a0fb18952c0
Neeraj Dalal <admin@nrjdalal.com>
2023-11-05 13:41:33 +0530

    updated display

--- src/app/(admin)/dashboard/shortUrls.tsx
+++ src/app/(admin)/dashboard/shortUrls.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { Eye } from 'lucide-react'
 import Link from 'next/link'
 import { getShortUrls } from './apis/shortUrls'
 
@@ -20,13 +21,14 @@ const Page = () => {
     <div className="mt-5 flex flex-col space-y-5">
       {data?.map((shortUrl: { id: string; url: string; visits: any }) => (
         <div
-          className="flex flex-col space-y-3 rounded-md border p-3 text-sm"
+          className="flex flex-col space-y-5 rounded-md border p-3 text-sm"
           key={shortUrl.id}
         >
           <div>
             <Link
-              href={`/${shortUrl.id}`}
               className="font-mono text-xs text-blue-500"
+              href={`/${shortUrl.id}`}
+              target="_blank"
             >
               rdt.li/{shortUrl.id}
             </Link>
@@ -36,8 +38,22 @@ const Page = () => {
             </Link>
           </div>
 
-          <div className="text-xs">
-            {shortUrl?.visits?.length || 'No visits yet!'}
+          <div className="flex justify-between text-xs">
+            <div className="flex items-end font-mono text-[8px] text-slate-500">
+              <p className="pt-0.5">
+                {shortUrl?.visits?.length
+                  ? `Last visited at ${new Date(
+                      shortUrl.visits[0],
+                    ).toLocaleString()}`
+                  : ''}
+              </p>
+            </div>
+            <div className="flex items-center gap-1 font-mono">
+              <Eye className="h-3 w-3 text-slate-600" />
+              <p className="pt-px">
+                {shortUrl?.visits?.length ? shortUrl.visits.length : 0}
+              </p>
+            </div>
           </div>
         </div>
       ))}
