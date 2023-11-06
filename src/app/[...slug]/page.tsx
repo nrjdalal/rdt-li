fe337bead5ee3d9b55ffedfff660ba1b0dc368db
Neeraj Dalal <admin@nrjdalal.com>
2023-11-06 22:59:32 +0530

    Update page.tsx

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -2,6 +2,7 @@ import { db } from '@/lib/db'
 import { shortUrls } from '@/lib/db/schema'
 import { eq } from 'drizzle-orm'
 import { permanentRedirect } from 'next/navigation'
+import Link from 'next/link'
 
 const Page = async ({ params }: { params: { slug: string } }) => {
   const slug = params.slug[0]
@@ -31,7 +32,7 @@ const Page = async ({ params }: { params: { slug: string } }) => {
   }
 
   return (
-    <p className="w-full border-b-2 p-5 text-center">No such URL exists.</p>
+    <p className="w-full border-b-2 p-5 text-center">No such URL exists. Create new <Link href="/">{process.env.NEXT_PUBLIC_APP_URL}</Link></p>
   )
 }
 
