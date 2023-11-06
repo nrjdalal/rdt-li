b75cca1ff2c850232bcb48eb018545daad3be0ce
Neeraj Dalal <admin@nrjdalal.com>
2023-11-07 04:25:10 +0530

    testing umami integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import { fontMono, fontSans } from '@/lib/fonts'
 import { cn } from '@/lib/utils'
 import type { Metadata } from 'next'
+import Head from 'next/head'
+import Script from 'next/script'
 import { Toaster } from 'sonner'
 import Provider from './provider'
 
@@ -29,6 +31,14 @@ export default function RootLayout({
           <Toaster expand={true} richColors />
         </Provider>
       </body>
+
+      {process.env.NEXT_PUBLIC_APP_URL === 'https://rdt.li' && (
+        <Script
+          async
+          src="https://umami-rdt-li.vercel.app/script.js"
+          data-website-id="27fe7c5f-3f22-4efa-8d44-aff3d5dadc51"
+        />
+      )}
     </html>
   )
 }
