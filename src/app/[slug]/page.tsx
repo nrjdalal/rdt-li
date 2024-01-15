4fb8719ff1e09a3c0f51f0cbfcca757d78092224
Neeraj Dalal <admin@nrjdalal.com>
2024-01-15 21:57:09 +0530

    v3 added api/redirect

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,10 +1,5 @@
-import { db } from '@/lib/db'
-import { shortUrls } from '@/lib/db/schema'
-import { smallDate } from '@/lib/utils'
-import { eq } from 'drizzle-orm'
 import Link from 'next/link'
 import { permanentRedirect } from 'next/navigation'
-import retry from 'p-retry'
 
 const Page = async ({ params }: { params: { slug: string } }) => {
   const slug = params.slug
@@ -19,30 +14,10 @@ const Page = async ({ params }: { params: { slug: string } }) => {
     permanentRedirect(data.redirect)
   }
 
-  if (data.message)
-    return (
-      <div className="flex h-[100dvh] flex-col items-center justify-center gap-4">
-        <p className="text-sm text-foreground/50">
-          {process.env.NEXT_PUBLIC_APP_URL?.split('://')[1]}/{slug}{' '}
-          {data.message}
-        </p>
-
-        <p className="text-sm">
-          Create short URLs at{' '}
-          <Link
-            className="animate-pulse text-sm font-bold text-orange-500 underline"
-            href="/"
-          >
-            {process.env.NEXT_PUBLIC_APP_URL?.split('://')[1]}
-          </Link>
-        </p>
-      </div>
-    )
-
   return (
     <div className="flex h-[100dvh] flex-col items-center justify-center gap-4">
       <p className="text-sm text-foreground/50">
-        Something went wrong. Please try again later.
+        {process.env.NEXT_PUBLIC_APP_URL?.split('://')[1]}/{slug}
       </p>
 
       <p className="text-sm">
