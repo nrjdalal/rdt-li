7878496747c27ad9a115b9df1c3fe0226e25b76d
Neeraj Dalal <admin@nrjdalal.com>
2024-01-15 22:06:18 +0530

    v3 added api/redirect

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,23 +1,26 @@
 import Link from 'next/link'
 import { permanentRedirect } from 'next/navigation'
 
-const Page = async ({ params }: { params: { slug: string } }) => {
-  const slug = params.slug
-
+async function getData(slug: string) {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_APP_URL}/api/redirect?slug=${slug}`,
   )
 
-  const data = await res.json()
+  return res.json()
+}
 
-  if (res.status === 303) {
+const Page = async ({ params }: { params: { slug: string } }) => {
+  const slug = params.slug
+  const data = await getData(slug)
+
+  if (data.redirect) {
     permanentRedirect(data.redirect)
   }
 
   return (
     <div className="flex h-[100dvh] flex-col items-center justify-center gap-4">
       <p className="text-sm text-foreground/50">
-        {process.env.NEXT_PUBLIC_APP_URL?.split('://')[1]}/{slug}
+        {process.env.NEXT_PUBLIC_APP_URL?.split('://')[1]}/{slug} {data.message}
       </p>
 
       <p className="text-sm">
